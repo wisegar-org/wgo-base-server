e7a44b173afd9d9f96d2f5b77a0657d1cdd5a863
Yariel Rodriguez <41107774+yarielre@users.noreply.github.com>
2023-03-29 09:38:18 +0200

    Update AuthResolver to protected methods

--- src/authentication/resolvers/AuthResolver.ts
+++ src/authentication/resolvers/AuthResolver.ts
@@ -68,8 +68,17 @@ export class AuthResolver {
     };
   }
 
+  /**
+   * @decorator @Mutation(() => LoginResponse, { name: AUTH_PATH_LOGIN })
+   * @param data
+   * @param ctx
+   * @returns
+   */
   @Mutation(() => LoginResponse, { name: AUTH_PATH_LOGIN })
-  async login(@Arg("data") data: LoginInput, @Ctx() ctx: IContextBase) {
+  protected async login(
+    @Arg("data") data: LoginInput,
+    @Ctx() ctx: IContextBase
+  ) {
     const authModel = new AuthModel({
       ...this.options,
       ctx,
@@ -78,8 +87,14 @@ export class AuthResolver {
     return login as LoginResponse;
   }
 
+  /**
+   * @decorator @Query(() => UserResponse, { name: AUTH_PATH_ME })
+   * @param data
+   * @param ctx
+   * @returns
+   */
   @Query(() => UserResponse, { name: AUTH_PATH_ME })
-  async me(@Arg("data") data: MeInput, @Ctx() ctx: IContextBase) {
+  protected async me(@Arg("data") data: MeInput, @Ctx() ctx: IContextBase) {
     const authModel = new AuthModel({
       ...this.options,
       ctx,
@@ -87,9 +102,17 @@ export class AuthResolver {
     const user = await authModel.me(data);
     return user as UserResponse;
   }
-
+  /**
+   * @decorator @Mutation(() => UserResponse, { name: AUTH_PATH_REGISTER })
+   * @param data
+   * @param ctx
+   * @returns
+   */
   @Mutation(() => UserResponse, { name: AUTH_PATH_REGISTER })
-  async register(@Arg("data") data: RegisterInput, @Ctx() ctx: IContextBase) {
+  protected async register(
+    @Arg("data") data: RegisterInput,
+    @Ctx() ctx: IContextBase
+  ) {
     const settingsModel = new SettingsModel(ctx);
     const config = (await settingsModel.getSettingsObject({
       type_settings: SETTINGS_SMTP,
@@ -110,9 +133,17 @@ export class AuthResolver {
     const user = await authModel.register(data as any);
     return user;
   }
-
+  /**
+   * @decorator @Mutation(() => UserResponse, { name: AUTH_PATH_EDIT_USER })
+   * @param data
+   * @param ctx
+   * @returns
+   */
   @Mutation(() => UserResponse, { name: AUTH_PATH_EDIT_USER })
-  async editUser(@Arg("data") data: EditUserInput, @Ctx() ctx: IContextBase) {
+  protected async editUser(
+    @Arg("data") data: EditUserInput,
+    @Ctx() ctx: IContextBase
+  ) {
     const settingsModel = new SettingsModel(ctx);
     const config = (await settingsModel.getSettingsObject({
       type_settings: SETTINGS_SMTP,
@@ -134,8 +165,14 @@ export class AuthResolver {
     return user;
   }
 
+  /**
+   * @decorator @Mutation(() => Boolean, { name: AUTH_PATH_RESEND_CONFIRMATION })
+   * @param data
+   * @param ctx
+   * @returns
+   */
   @Mutation(() => Boolean, { name: AUTH_PATH_RESEND_CONFIRMATION })
-  async resendConfirmation(
+  protected async resendConfirmation(
     @Arg("data") data: ResendConfirmationInput,
     @Ctx() ctx: IContextBase
   ) {
@@ -159,9 +196,14 @@ export class AuthResolver {
     const user = await authModel.resendConfirmation(data);
     return !!user;
   }
-
+  /**
+   * @decorator  @Mutation(() => Boolean, { name: AUTH_PATH_RESET_PASSWORD })
+   * @param data
+   * @param ctx
+   * @returns
+   */
   @Mutation(() => Boolean, { name: AUTH_PATH_RESET_PASSWORD })
-  async resetPassword(
+  protected async resetPassword(
     @Arg("data") data: ResendConfirmationInput,
     @Ctx() ctx: IContextBase
   ) {
@@ -186,8 +228,14 @@ export class AuthResolver {
     return !!resetResult;
   }
 
+  /**
+   * @decorator @Mutation(() => Boolean, { name: AUTH_PATH_CHANGE_RESET_PASSWORD })
+   * @param data
+   * @param ctx
+   * @returns
+   */
   @Mutation(() => Boolean, { name: AUTH_PATH_CHANGE_RESET_PASSWORD })
-  async changeResetPassword(
+  protected async changeResetPassword(
     @Arg("data") data: ResetPasswordInput,
     @Ctx() ctx: IContextBase
   ) {
@@ -199,8 +247,17 @@ export class AuthResolver {
     return resetResult;
   }
 
+  /**
+   * @decorator @Mutation(() => Boolean, { name: AUTH_PATH_CONFIRM_REGIST })
+   * @param data
+   * @param ctx
+   * @returns
+   */
   @Mutation(() => Boolean, { name: AUTH_PATH_CONFIRM_REGIST })
-  async confirmRegist(@Arg("data") data: MeInput, @Ctx() ctx: IContextBase) {
+  protected async confirmRegistration(
+    @Arg("data") data: MeInput,
+    @Ctx() ctx: IContextBase
+  ) {
     const authModel = new AuthModel({
       ...this.options,
       ctx,
@@ -209,8 +266,17 @@ export class AuthResolver {
     return !!user;
   }
 
+  /**
+   * @decorator @Query(() => UserResponse, { name: AUTH_PATH_GET_USER })
+   * @param data
+   * @param ctx
+   * @returns
+   */
   @Query(() => UserResponse, { name: AUTH_PATH_GET_USER })
-  async getUser(@Arg("data") data: IdInput, @Ctx() ctx: IContextBase) {
+  protected async getUser(
+    @Arg("data") data: IdInput,
+    @Ctx() ctx: IContextBase
+  ) {
     const userRolesModel = new UserRolesModel({
       ...this.options,
       ctx,
@@ -219,8 +285,13 @@ export class AuthResolver {
     return user;
   }
 
+  /**
+   * @decorator @Query(() => [UserResponse], { name: AUTH_PATH_GET_ALL_USERS })
+   * @param ctx
+   * @returns
+   */
   @Query(() => [UserResponse], { name: AUTH_PATH_GET_ALL_USERS })
-  async getAllUsers(@Ctx() ctx: IContextBase) {
+  protected async getAllUsers(@Ctx() ctx: IContextBase) {
     const userRolesModel = new UserRolesModel({
       ...this.options,
       ctx,
@@ -229,8 +300,13 @@ export class AuthResolver {
     return users;
   }
 
+  /**
+   * @decorator @Query(() => [String], { name: AUTH_PATH_GET_ALL_ROLES })
+   * @param ctx
+   * @returns
+   */
   @Query(() => [String], { name: AUTH_PATH_GET_ALL_ROLES })
-  async getAllRoles(@Ctx() ctx: IContextBase) {
+  protected async getAllRoles(@Ctx() ctx: IContextBase) {
     const userRolesModel = new UserRolesModel({
       ...this.options,
       ctx,
@@ -239,8 +315,17 @@ export class AuthResolver {
     return roles;
   }
 
+  /**
+   * @decorator @Mutation(() => Boolean, { name: AUTH_PATH_DELETE_USER })
+   * @param data
+   * @param ctx
+   * @returns
+   */
   @Mutation(() => Boolean, { name: AUTH_PATH_DELETE_USER })
-  async deleteUser(@Arg("data") data: IdInput, @Ctx() ctx: IContextBase) {
+  protected async deleteUser(
+    @Arg("data") data: IdInput,
+    @Ctx() ctx: IContextBase
+  ) {
     const userRolesModel = new UserRolesModel({
       ...this.options,
       ctx,
@@ -249,8 +334,14 @@ export class AuthResolver {
     return !!user;
   }
 
+  /**
+   * @decorator @Query(() => Boolean, { name: AUTH_PATH_CHECK_USER_NAME })
+   * @param data
+   * @param ctx
+   * @returns
+   */
   @Query(() => Boolean, { name: AUTH_PATH_CHECK_USER_NAME })
-  async validUserName(
+  protected async validUserName(
     @Arg("data") data: ValidUserNameInput,
     @Ctx() ctx: IContextBase
   ) {
@@ -262,9 +353,18 @@ export class AuthResolver {
     return result;
   }
 
+  /**
+   * @decorator @Authorized() | @Query(() => HistoricResponse, { name: AUTH_PATH_GET_HISTORIC })
+   * @param id
+   * @param ctx
+   * @returns
+   */
   @Authorized()
   @Query(() => HistoricResponse, { name: AUTH_PATH_GET_HISTORIC })
-  async getUserHistoric(@Arg("id") id: number, @Ctx() ctx: IContextBase) {
+  protected async getUserHistoric(
+    @Arg("id") id: number,
+    @Ctx() ctx: IContextBase
+  ) {
     const historyService = new HistoricModel(UserEntity, ctx);
     const result = await historyService.getHistoric(id);
     return result.map((historic: any) =>
@@ -272,19 +372,29 @@ export class AuthResolver {
     );
   }
 
+  /**
+   * @decorator @Authorized() | @Query(() => [HistoricResponse], { name: AUTH_PATH_GET_ALL_HISTORIC })
+   * @param ctx
+   * @returns
+   */
   @Authorized()
   @Query(() => [HistoricResponse], { name: AUTH_PATH_GET_ALL_HISTORIC })
-  async getAllUserHistoric(@Ctx() ctx: IContextBase) {
+  protected async getAllUserHistoric(@Ctx() ctx: IContextBase) {
     const historyService = new HistoricModel(UserEntity, ctx);
     const result = await historyService.getAllHistoric();
     return result.map((historic: any) =>
       HistoricModel.ParseHistoricResponse(historic)
     );
   }
-
+  /**
+   * @decorator @Authorized() | @Query(() => [HistoricResponse], { name: AUTH_PATH_GET_ALL_HISTORIC_BY_USER })
+   * @param id
+   * @param ctx
+   * @returns
+   */
   @Authorized()
   @Query(() => [HistoricResponse], { name: AUTH_PATH_GET_ALL_HISTORIC_BY_USER })
-  async getUserAllHistoricByUser(
+  protected async getUserAllHistoricByUser(
     @Arg("id") id: number,
     @Ctx() ctx: IContextBase
   ) {
