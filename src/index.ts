9a783e4d6378e123d62076e17ca46b44ca19eb08
Yariel Rodriguez <yariel.re@gmail.com>
2024-09-21 01:22:16 +0200

    Remove run export func

--- src/index.ts
+++ src/index.ts
@@ -44,87 +44,87 @@ export * from "./template";
 export * from "./translation";
 // export * from "./wgo";
 
-export async function run(app: any) {
-  const port = GetPortKey();
+// export async function run(app: any) {
+//   const port = GetPortKey();
 
-  const options: IServerOptions = {
-    app: app,
-    authenticator: AuthenticationHandler,
-    context: AppContextHandler,
-    formatError: errorHandler,
-    controllers: getControllers(),
-    port: parseInt(port),
-    maxFileSize: 5000000000,
-    maxFiles: 10,
-    useCors: true,
-    middlewares: (app: any) => {
-      // UseHostAdminMiddleware(app);
-      // UseTemplatingMiddleware(app);
-      UseStaticMediaFilesMiddleware(app);
-      UseRestMiddleware(options);
-    },
-    resolvers: getResolvers(),
-    privateKey: GetPrivateKey(),
-    publicKey: GetPublicKey(),
-    expiresIn: GetExpiresInKey(),
-    expirationFreq: ExpirationFreqEnum.Low,
-    gqlValidateSettings: {
-      forbidUnknownValues: false,
-    },
-  };
+//   const options: IServerOptions = {
+//     app: app,
+//     authenticator: AuthenticationHandler,
+//     context: AppContextHandler,
+//     formatError: errorHandler,
+//     controllers: getControllers(),
+//     port: parseInt(port),
+//     maxFileSize: 5000000000,
+//     maxFiles: 10,
+//     useCors: true,
+//     middlewares: (app: any) => {
+//       // UseHostAdminMiddleware(app);
+//       // UseTemplatingMiddleware(app);
+//       UseStaticMediaFilesMiddleware(app);
+//       UseRestMiddleware(options);
+//     },
+//     resolvers: getResolvers(),
+//     privateKey: GetPrivateKey(),
+//     publicKey: GetPublicKey(),
+//     expiresIn: GetExpiresInKey(),
+//     expirationFreq: ExpirationFreqEnum.Low,
+//     gqlValidateSettings: {
+//       forbidUnknownValues: false,
+//     },
+//   };
 
-  options.app.use(express.json());
+//   options.app.use(express.json());
 
-  options.expirationFreq = options.expirationFreq
-    ? options.expirationFreq
-    : ExpirationFreqEnum.Normal;
+//   options.expirationFreq = options.expirationFreq
+//     ? options.expirationFreq
+//     : ExpirationFreqEnum.Normal;
 
-  console.debug("Registering Cors middleware");
-  UseCorsMiddleware(options);
+//   console.debug("Registering Cors middleware");
+//   UseCorsMiddleware(options);
 
-  console.debug("Registering Jwt middleware");
-  UseJwtMiddleware(options);
+//   console.debug("Registering Jwt middleware");
+//   UseJwtMiddleware(options);
 
-  if (options.controllers && options.controllers.length > 0) {
-    console.debug("Registering Rest middleware");
-    UseRestMiddleware(options);
-  }
+//   if (options.controllers && options.controllers.length > 0) {
+//     console.debug("Registering Rest middleware");
+//     UseRestMiddleware(options);
+//   }
 
-  if (options.resolvers && options.resolvers.length > 0) {
-    console.debug("Registering Graphql middleware");
-    UseGqlServer(options);
-  }
+//   if (options.resolvers && options.resolvers.length > 0) {
+//     console.debug("Registering Graphql middleware");
+//     UseGqlServer(options);
+//   }
 
-  if (options.middlewares) {
-    console.debug("Registering Extras middleware");
-    options.middlewares(options.app);
-  }
+//   if (options.middlewares) {
+//     console.debug("Registering Extras middleware");
+//     options.middlewares(options.app);
+//   }
 
-  await createDatabase({
-    ifNotExist: true,
-    options: {
-      ...dataSourceOptions,
-      migrationsRun: false,
-      entities: [],
-      migrations: [],
-    },
-  });
-  const dataSource = await PostgresDataSource.initialize();
-  if (!dataSourceOptions.migrationsRun) {
-    dataSource.runMigrations();
-  }
+//   await createDatabase({
+//     ifNotExist: true,
+//     options: {
+//       ...dataSourceOptions,
+//       migrationsRun: false,
+//       entities: [],
+//       migrations: [],
+//     },
+//   });
+//   const dataSource = await PostgresDataSource.initialize();
+//   if (!dataSourceOptions.migrationsRun) {
+//     dataSource.runMigrations();
+//   }
 
-  //Init db settings
-  await settingsSeeder(dataSource);
+//   //Init db settings
+//   await settingsSeeder(dataSource);
 
-  //Core Seeders
-  await roleSuperAdminSeeder(dataSource); //create superadmin rol
-  await userAdminSeeder(dataSource); //create admin user with superadmin rol
-  await languageDefaultSeeder(dataSource); //create default language
-  await mediaPublicSeeder({ ...ctx, dataSource }); //export public media files
+//   //Core Seeders
+//   await roleSuperAdminSeeder(dataSource); //create superadmin rol
+//   await userAdminSeeder(dataSource); //create admin user with superadmin rol
+//   await languageDefaultSeeder(dataSource); //create default language
+//   await mediaPublicSeeder({ ...ctx, dataSource }); //export public media files
 
-  // Loop function
-  setTimeout(async () => {
-    // loopUpdateIssues();
-  }, 0);
-}
+//   // Loop function
+//   setTimeout(async () => {
+//     // loopUpdateIssues();
+//   }, 0);
+// }
