72784dad808b0d1c0efab3dd8dd0bcab429a81a1
Yariel Rodriguez <yariel.re@gmail.com>
2025-01-18 15:33:46 +0100

    Fix WGEmailResponse empty field error

--- src/email/resolvers/WGEmailResponses.ts
+++ src/email/resolvers/WGEmailResponses.ts
@@ -1,6 +1,9 @@
-import { ObjectType } from "type-graphql";
+import { Field, ObjectType } from "type-graphql";
 import { WGEmail } from "./WGEmailInputs";
 import { GenericResponse } from "../../core/resolvers/CoreResponses";
 
 @ObjectType()
-export class WGEmailResponse extends GenericResponse(WGEmail) {}
+export class WGEmailResponse extends GenericResponse(WGEmail) {
+  @Field(() => String, { nullable: false })
+  test?: string;
+}
