4634ef104edb902fdec64ea3532f75d716c692d8
Yariel Rodriguez <yariel.re@gmail.com>
2025-01-31 23:42:11 +0100

    Update client util service

--- src-client/src/services/UtilService.ts
+++ src-client/src/services/UtilService.ts
@@ -4,6 +4,8 @@ import dayjs from "dayjs";
 dayjs.extend(customParseFormat);
 import currency from "currency.js";
 import { MenuListItem } from "@wisegar-org/wgo-base-models/build/core/Menu";
+import { openURL } from "quasar";
+import { EventClassOption } from "src/models/Events";
 
 export const UtilService = {
   parseDate(date: Date | string, format: string = "DD/MM/YYYY HH:mm") {
@@ -54,5 +56,26 @@ export const UtilService = {
     const re =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
+  },
+  openNewTab(evt: Event, url: string) {
+    evt.preventDefault();
+    if (location.href.includes("/#")) {
+      openURL(`/#${url}`);
+    } else {
+      openURL(url);
+    }
+  },
+  openNewTabUrl(url: string) {
+    if (location.href.includes("/#")) {
+      openURL(`/#${url}`);
+    } else {
+      openURL(url);
+    }
+  },
+  getDefaultClass() {
+    const date = new Date();
+    const year = `${date.getFullYear() + (date.getMonth() > 7 ? 0 : -1)}`;
+    const filterYears = EventClassOption.filter((cls) => cls.startsWith(year));
+    return filterYears.length > 0 ? filterYears[0] : EventClassOption[0];
   },
 };
